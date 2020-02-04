b31b2cc9f63b2912f2f6feab1312a4bd172a2019
MagLaszlo <maglaszlo75@gmail.com>
2020-02-04 22:53:03 +0100

    #2: ASP.NET MVC alkalmazás Identity használatával - kezdőkép

---
+++ OoPRestaurant202002/OoPRestaurant202002/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OoPRestaurant202002.MvcApplication" Language="C#" %>
---
+++ OoPRestaurant202002/OoPRestaurant202002/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OoPRestaurant202002.Startup))]
+namespace OoPRestaurant202002
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
